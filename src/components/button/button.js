7b78373f94da6d3f6a195a7884aa708b1fb40f14
Yaroslav Illyashenko <y.illyashenko@prismsolutions.ru>
2020-10-15 00:42:16 +0300

    feat: implement button component in storyook

--- src/components/button/button.js
+++ src/components/button/button.js
@@ -1,7 +1,39 @@
 import React from "react";
+import cn from "classnames";
+
+import IconContainer from "../icon-container/icon-container";
 
 import "./button.css";
 
-const Button = () => <button className="Button">Button</button>;
+const Icon = ({ icon }) => (
+  <span className="Button-icon">
+    <IconContainer>{icon}</IconContainer>
+  </span>
+);
+
+const Button = ({
+  title,
+  component: Component = "button",
+  prependIcon,
+  appendIcon,
+  isPrimary,
+  isDisabled,
+  ...rest
+}) => (
+  <Component
+    className={cn("Button", {
+      "Button--primary": isPrimary,
+      "Button--disabled": isDisabled,
+    })}
+    disabled={isDisabled}
+    {...rest}
+  >
+    {prependIcon && <Icon icon={prependIcon} />}
+
+    <span className="Button-title">{title}</span>
+
+    {appendIcon && <Icon icon={appendIcon} />}
+  </Component>
+);
 
 export default Button;
--- src/stories/button.js
+++ src/stories/button.js
@@ -1,7 +1,17 @@
 import React from "react";
+import { text, boolean } from "@storybook/addon-knobs";
 
 import Button from "../components/button/button";
+import { IconAngleArrowRight } from "../components/icons";
 
-export const main = () => <Button />;
+export const main = () => (
+  <Button
+    title={text("Title", "Some text")}
+    prependIcon={boolean("Prepend icon") ? <IconAngleArrowRight /> : null}
+    appendIcon={boolean("Append icon") ? <IconAngleArrowRight /> : null}
+    isPrimary={boolean("Primary")}
+    isDisabled={boolean("Disabled")}
+  />
+);
 
 export default { title: "Button" };
