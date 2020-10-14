e4ceff397cc3b3db3418b74a90294d50ac27976f
Yaroslav Illyashenko <y.illyashenko@prismsolutions.ru>
2020-10-15 00:41:57 +0300

    feat: install storybook addon knobs

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,8 @@
 module.exports = {
   stories: ["../src/stories/*.@(js|jsx|ts|tsx)"],
-  addons: ["@storybook/addon-essentials", "@storybook/preset-create-react-app"],
+  addons: [
+    "@storybook/addon-essentials",
+    "@storybook/preset-create-react-app",
+    "@storybook/addon-knobs",
+  ],
 };
