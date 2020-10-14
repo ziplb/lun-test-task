3c5e736a8a3836185d079381aa58d99bb1d3f44a
Yaroslav Illyashenko <y.illyashenko@prismsolutions.ru>
2020-10-14 23:17:00 +0300

    feat: implement angle arrow right

---
+++ src/components/icons/angle-arrow-right/angle-arrow-right.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import Svg from "../svg/svg";
+
+import "./angle-arrow-right.css";
+
+const IconAngleArrowRight = () => (
+  <Svg className="IconAngleArrowRight">
+    <path d="M12.6 24l6.9-8-6.9-8" />
+  </Svg>
+);
+
+export default IconAngleArrowRight;
---
+++ src/components/icons/index.js
@@ -0,0 +1,1 @@
+export { default as IconAngleArrowRight } from "./angle-arrow-right/angle-arrow-right";
