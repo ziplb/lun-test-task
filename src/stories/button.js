d812891480b67e4a853b793ad0e7e12fd93af83e
Yaroslav Illyashenko <y.illyashenko@prismsolutions.ru>
2020-10-15 23:06:17 +0300

    fix: change prepend icon from IconAngleArrowRight to IconAngleArrowLeft in button story

--- src/stories/button.js
+++ src/stories/button.js
@@ -2,12 +2,12 @@ import React from "react";
 import { text, boolean } from "@storybook/addon-knobs";
 
 import Button from "../components/button/button";
-import { IconAngleArrowRight } from "../components/icons";
+import { IconAngleArrowRight, IconAngleArrowLeft } from "../components/icons";
 
 export const main = () => (
   <Button
     title={text("Title", "Some text")}
-    prependIcon={boolean("Prepend icon") ? <IconAngleArrowRight /> : null}
+    prependIcon={boolean("Prepend icon") ? <IconAngleArrowLeft /> : null}
     appendIcon={boolean("Append icon") ? <IconAngleArrowRight /> : null}
     isPrimary={boolean("Primary")}
     isDisabled={boolean("Disabled")}
