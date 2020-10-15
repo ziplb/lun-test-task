e9b4f0310c391936e53e4c8ba7fc50501611f06f
Yaroslav Illyashenko <y.illyashenko@prismsolutions.ru>
2020-10-15 23:05:28 +0300

    feat: set stroke width and fill color on default

--- src/components/icons/filled-angle-arrow-top/filled-angle-arrow-top.js
+++ src/components/icons/filled-angle-arrow-top/filled-angle-arrow-top.js
@@ -2,8 +2,6 @@ import React from "react";
 
 import Svg from "../svg/svg";
 
-import "./filled-angle-arrow-top.css";
-
 const IconFilledAngleArrowTop = () => (
   <Svg className="IconFilledAngleArrowTop">
     <path d="M24.1 19.4l-8-6.9-8 6.9" />
