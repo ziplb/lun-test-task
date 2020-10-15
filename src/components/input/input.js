8df0d78bfb310587f186d7cdf6df117efa5eb574
Yaroslav Illyashenko <y.illyashenko@prismsolutions.ru>
2020-10-15 23:17:24 +0300

    feat: implement the ability to pass icon to input component

--- src/components/input/input.js
+++ src/components/input/input.js
@@ -1,16 +1,27 @@
 import React from "react";
 import cn from "classnames";
 
+import IconContainer from "../icon-container/icon-container";
+
 import "./input.css";
 
-const Input = ({ isError, isSmall, ...rest }) => (
-  <input
-    className={cn("Input", {
-      "Input--error": isError,
-      "Input--small": isSmall,
-    })}
-    {...rest}
-  />
+const Input = ({ icon, isError, isSmall, ...rest }) => (
+  <div className="Input">
+    <input
+      className={cn("Input-el", {
+        "Input-el--error": isError,
+        "Input-el--small": isSmall,
+        "Input-el--withRightGap": icon,
+      })}
+      {...rest}
+    />
+
+    {icon && (
+      <div className="Input-icon">
+        <IconContainer>{icon}</IconContainer>
+      </div>
+    )}
+  </div>
 );
 
 export default Input;
--- src/stories/input.js
+++ src/stories/input.js
@@ -2,10 +2,12 @@ import React from "react";
 import { boolean } from "@storybook/addon-knobs";
 
 import Input from "../components/input/input";
+import { IconFilledAngleArrowDown } from "../components/icons";
 
 export const main = () => (
   <Input
     placeholder="Some placeholder"
+    icon={boolean("Icon") ? <IconFilledAngleArrowDown /> : null}
     isSmall={boolean("Small")}
     isError={boolean("Error")}
   />
