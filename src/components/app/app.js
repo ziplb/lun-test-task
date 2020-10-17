7924222e10a24ed766820da2211a93236d78bf69
Yaroslav Illyashenko <y.illyashenko@prismsolutions.ru>
2020-10-17 13:43:56 +0300

    feat: implement application basic routing

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,7 +1,23 @@
 import React from "react";
+import { Switch, Route, Redirect } from "react-router-dom";
+
+import { getQuestionnaireLink, getOverviewLink } from "../../routes";
 
 import "./app.css";
 
-const App = () => <div className="App">Hello, world!</div>;
+const AppScene = () => (
+  <Switch>
+    <Route path={getOverviewLink()} component={() => <>Overview</>} exact />
+    <Route path={getQuestionnaireLink()} component={() => <>Questionnaire</>} />
+
+    <Redirect to={getQuestionnaireLink()} />
+  </Switch>
+);
+
+const App = () => (
+  <div className="App">
+    <AppScene />
+  </div>
+);
 
 export default App;
