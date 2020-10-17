39ffb8b6551e3876a89f665df8f9eb829f4708e8
Yaroslav Illyashenko <y.illyashenko@prismsolutions.ru>
2020-10-17 13:46:26 +0300

    feat: create questionnaire component

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,13 +1,15 @@
 import React from "react";
 import { Switch, Route, Redirect } from "react-router-dom";
 
+import Questionnaire from "../questionnaire/questionnaire";
+
 import { getQuestionnaireLink, getOverviewLink } from "../../routes";
 
 import "./app.css";
 
 const AppScene = () => (
   <Switch>
-    <Route path={getQuestionnaireLink()} component={() => <>Questionnaire</>} />
+    <Route path={getQuestionnaireLink()} component={Questionnaire} />
     <Route path={getOverviewLink()} component={() => <>Overview</>} exact />
 
     <Redirect to={getQuestionnaireLink()} />
---
+++ src/components/questionnaire/questionnaire.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+import "./questionnaire.css";
+
+const Questionnaire = () => <div className="Questionnaire">Questionnaire</div>;
+
+export default Questionnaire;
