4fb4509f57156ccec8184788e7fb519aad4fc78a
Yaroslav Illyashenko <y.illyashenko@prismsolutions.ru>
2020-10-17 13:37:09 +0300

    feat: style app component, move it to components directory

--- src/App.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-const App = () => <div>Create react app!</div>;
-
-export default App;
---
+++ src/components/app/app.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+import "./app.css";
+
+const App = () => <div className="App">Hello, world!</div>;
+
+export default App;
