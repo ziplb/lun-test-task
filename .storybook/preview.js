3c28eab82650aa9e82bf89ad34a4ee64890bb77b
Yaroslav Illyashenko <y.illyashenko@prismsolutions.ru>
2020-10-17 16:16:22 +0300

    feat: add router as a decorator to storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,1 +1,7 @@
+import React from "react";
+import { addDecorator } from "@storybook/react";
+import { BrowserRouter as Router } from "react-router-dom";
+
 import "../src/index.css";
+
+addDecorator((story) => <Router>{story()}</Router>);
