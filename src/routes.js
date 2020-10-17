c8534a7f36595b3ab3ce333d66162ce254f81331
Yaroslav Illyashenko <y.illyashenko@prismsolutions.ru>
2020-10-17 13:40:06 +0300

    feat: create routes

---
+++ src/routes.js
@@ -0,0 +1,5 @@
+export const getIndexLink = () => "/";
+export const getQuestionnaireLink = () => "/questionnaire";
+export const getQuestionnaireStepLink = (step) =>
+  `/${getQuestionnaireLink()}/${step}`;
+export const getOverviewLink = () => "/overview";
