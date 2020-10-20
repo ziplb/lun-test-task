9f7cc59371870437d716a6f0707e8e36ebbf5d17
Yaroslav Illyashenko <y.illyashenko@prismsolutions.ru>
2020-10-20 22:20:48 +0300

    feat: implement filtering values

--- src/components/autocomplete/autocomplete.js
+++ src/components/autocomplete/autocomplete.js
@@ -5,18 +5,23 @@ import Input from "../input/input";
 import "./autocomplete.css";
 
 const Autocomplete = ({
+  value,
   optionList,
   renderOption = ({ title }) => title,
   ...rest
 }) => {
+  const filteredValues = optionList.filter(({ title }) =>
+    title.toLowerCase().includes(value.toLowerCase())
+  );
+
   return (
     <div className="Autocomplete">
       <div className="Autocomplete-input">
-        <Input {...rest} />
+        <Input value={value} {...rest} />
       </div>
 
       <div className="Autocomplete-optionList">
-        {optionList.map((option) => (
+        {filteredValues.map((option) => (
           <div key={option.value} className="Autocomplete-optionItem">
             <div className="Autocomplete-option">{renderOption(option)}</div>
           </div>
