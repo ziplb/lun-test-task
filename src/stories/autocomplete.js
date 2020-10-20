447f2fe253756a9a32858ce7d27a8663b668fccc
Yaroslav Illyashenko <y.illyashenko@prismsolutions.ru>
2020-10-20 22:12:07 +0300

    feat: implement simple store for autocomplete storie

--- src/stories/autocomplete.js
+++ src/stories/autocomplete.js
@@ -1,10 +1,22 @@
-import React from "react";
+import React, { useState, cloneElement } from "react";
 
 import Autocomplete from "../components/autocomplete/autocomplete";
 import { optionListSeeds } from "./seeds";
 
+const Store = ({ children }) => {
+  const [value, setValue] = useState("");
+
+  return cloneElement(children, {
+    value,
+    getValue: (e) => e.target.value,
+    onChange: setValue,
+  });
+};
+
 export const main = () => (
-  <Autocomplete placeholder="Some placeholder" optionList={optionListSeeds} />
+  <Store>
+    <Autocomplete placeholder="Some placeholder" optionList={optionListSeeds} />
+  </Store>
 );
 
 export default { title: "Autocomplete" };
