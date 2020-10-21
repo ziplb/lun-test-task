bbf80a6da3882de2413bf1f5dbdfbb57a5112acf
Yaroslav Illyashenko <y.illyashenko@prismsolutions.ru>
2020-10-21 19:56:58 +0300

    fix: limit animal radio width in storybook

--- src/stories/animal-radio.js
+++ src/stories/animal-radio.js
@@ -1,9 +1,13 @@
 import React from "react";
 
+import { ControlSize } from ".storybook/decorators";
 import AnimalRadio from "../components/animal-radio/animal-radio";
 
 import { animalList } from "../data";
 
 export const main = () => <AnimalRadio image={animalList[0].image} />;
 
-export default { title: "AnimalRadio" };
+export default {
+  title: "AnimalRadio",
+  decorators: [(story) => <ControlSize story={story} width={160} />],
+};
