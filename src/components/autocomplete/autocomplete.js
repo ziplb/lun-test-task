23d916b088dbbde27dd3e05176df0bd578286320
Yaroslav Illyashenko <y.illyashenko@prismsolutions.ru>
2020-10-21 14:12:42 +0300

    fix: add option list element existing checking

--- src/components/autocomplete/autocomplete.js
+++ src/components/autocomplete/autocomplete.js
@@ -37,7 +37,7 @@ const Autocomplete = ({
   }, [selectedOptionEl]);
 
   useLayoutEffect(() => {
-    if (!highlightedOptionEl || !highlightedOptionEl) {
+    if (!optionListEl || !highlightedOptionEl) {
       return;
     }
 
